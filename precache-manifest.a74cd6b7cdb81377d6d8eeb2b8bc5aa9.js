a584c19c023438d0c8990a9369bd40c1b77e0a3b
Peter Lodri <peterl@dev.inpedio.com>
2020-02-03 13:03:32 +0100

    Updates

---
+++ precache-manifest.a74cd6b7cdb81377d6d8eeb2b8bc5aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b233e7c358238d9df66cc812e7205921",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "0fd925aee59c3c519c80",
+    "url": "/birthday/static/css/main.eeac728e.chunk.css"
+  },
+  {
+    "revision": "9ab3ff16d88f03d21085",
+    "url": "/birthday/static/js/2.dcfa49a5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/birthday/static/js/2.dcfa49a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd925aee59c3c519c80",
+    "url": "/birthday/static/js/main.507712bc.chunk.js"
+  },
+  {
+    "revision": "4786df56ccda1ed22244",
+    "url": "/birthday/static/js/runtime-main.c3ad665b.js"
+  }
+]);
